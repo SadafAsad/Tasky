db9419d51d24569397169c24af0e43169803b924
SadafAsad <sadaf98x@gmail.com>
2019-12-03 13:27:37 +0330

    tasky proj added

---
+++ app/src/main/java/com/example/sadafx/tasky_proj/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.sadafx.tasky_proj;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
