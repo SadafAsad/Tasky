d1d1a4ddbce331157574c29a6e690cb533b238ed
sadaf <sadaf98x@gmail.com>
2020-01-23 10:57:12 +0330

    DualProgressView added

---
+++ app/src/main/java/com/example/sadafx/tasky_proj/DualProgressView.java
@@ -0,0 +1,40 @@
+package com.example.sadafx.tasky_proj;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class DualProgressView extends View {
+    public DualProgressView(Context context) {
+        super(context);
+    }
+
+    public DualProgressView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DualProgressView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+    }
+}
